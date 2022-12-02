1e71f7201fe11000dd6f09b1d76bce38300d2982
Madi niakate <madi.niakate@accenture.com>
2022-12-02 14:54:45 +0100

    feat display articles in home page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ArticleRepository;
+
+#[Route('/')]
+class HomeController extends AbstractController
+{
+    #[Route('', name: 'app_home')]
+    public function index(ArticleRepository $articleRepository): Response
+    {
+
+        $articles = $articleRepository->findAll();
+        
+        return $this->render('home/index.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+}
