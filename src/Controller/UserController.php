cc56617b1db222877e50c78545f8966fe2341d42
Madi niakate <madi.niakate@accenture.com>
2022-12-03 18:30:51 +0100

    feat update profile password

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,6 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\UserType;
+use App\Form\UserPasswordFormType;
+use App\Repository\UserRepository;
+use App\Entity\User;
+
 
 class UserController extends AbstractController
 {
@@ -20,4 +26,42 @@ class UserController extends AbstractController
             'products' => $products
         ]);
     }
+
+    #[Route('profile/{id}/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
+    public function editProfile(Request $request, User $user, UserRepository $userRepository): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userRepository->save($user, true);
+
+           // return $this->redirectToRoute('app_user_test_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('content/user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('profile/{id}/password/edit', name: 'app_profile_password_edit', methods: ['GET', 'POST'])]
+    public function editPassword(Request $request, User $user, UserRepository $userRepository): Response
+    {
+        $form = $this->createForm(UserPasswordFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userRepository->save($user, true);
+
+           // return $this->redirectToRoute('app_user_test_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('content/user/edit.password.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+   
 }
