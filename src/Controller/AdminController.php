c712b0fe9842a6f92c70cced21dad1dcafe72d44
Madi niakate <madi.niakate@accenture.com>
2022-12-04 10:45:05 +0100

    feat display number of category,user,product and article on admin page

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,14 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ArticleRepository;
+use App\Repository\UserRepository;
+use App\Repository\ProductRepository;
+use App\Repository\CategoryRepository;
 
 class AdminController extends AbstractController
 {
+
+    // Permet de récupérer le nombre d'article, category, produits et utilisateur du site
     #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    public function getWebsiteStatistics(CategoryRepository $categoryRepository,ArticleRepository $articleRepository,ProductRepository $productRepository,UserRepository $userRepository): Response
     {
+
+        $articlesCount = count($articleRepository->findAll());
+        $productsCount = count($productRepository->findAll());
+        $categoryCount = count($categoryRepository->findAll());
+        $usersCount = count($userRepository->findAll());
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'articlesCount' => $articlesCount,
+            'productsCount' => $productsCount,
+            'categoryCount' => $categoryCount,
+            'usersCount' => $usersCount,
         ]);
     }
 }
